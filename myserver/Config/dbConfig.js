4dde37aac72c03d7c21069d2c1bbed8ad725bebc
MAY <2660397441@qq.com>
2019-09-18 11:59:58 +0800

    复制服务器代码

---
+++ myserver/Config/dbConfig.js
@@ -0,0 +1,10 @@
+// 数据库的基本配置
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'h519061',
+    port: 3307,
+    /*执行多条sql语句 */
+    multipleStatements: true
+}
---
+++ myserver/Controller/UserController.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const UserModel = require('../Model/UserModel');
+const router = express.Router();
+
+router.post('/reg', (req, res)=>{
+    console.log(req.body);//object
+    // 需要对数据进行操作  Model 
+    let userModel  = new UserModel();
+    // 对数据进行处理 passwd1
+    delete req.body.passwd1;
+    // req.body.regtimes = new Date().toLocaleString();
+
+    userModel.registerUser(req.body, (results)=>{
+        console.log(results);
+        if(results.insertId){
+            res.json({code:1, Msg:'注册成功'});
+        }else{
+            res.json({code:-1, Msg:'注册失败，请重新操作'});
+        }
+    });
+});
+
+router.post('/login', (req, res)=>{
+    let userModel  = new UserModel();
+    let username = req.body.username;
+    let passwd = req.body.passwd;
+
+    userModel.getUserByUsername(username, (results)=>{
+            let ob = {
+                code:1,
+                Msg:'登录成功'
+            };
+            if(results.length == 0){
+                ob = {
+                    code:-1,
+                    Msg:'用户不存在'
+                };
+            }else if(results[0].passwd != passwd){
+                ob = {
+                    code:0,
+                    Msg:'密码错误'
+                };
+            }
+            res.json(ob);
+    });
+});
+
+module.exports = router;
