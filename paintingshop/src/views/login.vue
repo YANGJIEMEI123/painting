eff5e8b5e5c2bcc3833d17ef70860aee050515a3
2512230445 <2512230445@qq.com>
2019-09-20 09:00:00 +0800

    注册

---
+++ paintingshop/src/views/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <form action="">
+            <label>账号</label><input type="text" name="account">
+             <label>昵称</label><input type="text" name="nickname">
+              <label>密码</label><input type="text" name="passwd">
+              <input type="button" value="登录">
+              <input type="button" value="取消">
+        </form>
+    </div>
+</template>
+<script></script>
+<style>
+
+</style>
